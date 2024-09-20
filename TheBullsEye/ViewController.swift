f0db1fc6dad9ecbc730dcc70cfa2b45836123ed0
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-09-20 15:10:23 +0500

    Initial Commit

---
+++ TheBullsEye/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheBullsEye
+//
+//  Created by Bakhrom Usmanov on 20/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
